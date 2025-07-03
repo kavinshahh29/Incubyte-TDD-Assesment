6e78e5745286fba1aa89b8f3fca6bd118865026d
Kavin <kavinshah290304@gmail.com>
2025-07-04 00:28:24 +0530

    Setup Maven Project And JUnit

---
+++ IncubyteTDDAssesment/src/main/java/org/kavinshah/StringCalculator.java
@@ -0,0 +1,4 @@
+package org.kavinshah;
+
+public class StringCalculator {
+}
---
+++ IncubyteTDDAssesment/src/test/java/org/kavinshah/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package org.kavinshah;
+
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+    @Test
+    public void test() {
+
+    }
+}
